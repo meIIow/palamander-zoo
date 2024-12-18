1638810fbf78f66af5bd23ef0d3cae023ba0ae9c
meIIow <maxwell.oliver.white@gmail.com>
2024-12-17 22:12:40 -0800

    Move demo code into dedicated directory

--- client/src/popup.tsx
+++ client/src/popup.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './demo/index.css'
+import App from './demo/App.tsx'
 
 (async () => {
   const response = await chrome.runtime.sendMessage({greeting: "hello"});
