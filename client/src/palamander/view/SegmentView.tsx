88676bfcbd71b1e95cd4fb415b49a0a91a1d90bc
meIIow <maxwell.oliver.white@gmail.com>
2024-12-25 00:26:30 -0800

    Fix: Palamander wraparound on negative modulo
    Fix an issue where the Pal sometimes leaves the screen going down/right. Since modulo does nothing to negative numbers in JS, the old wraparound logic was not working.

--- client/src/palamander/view/SegmentView.tsx
+++ client/src/palamander/view/SegmentView.tsx
@@ -7,10 +7,18 @@ type SegmentViewProps = {
   magnification: number
 }
 
+function edgeAsPixels(center: number, radius: number, magnification: number) {
+  return (center - radius)*magnification/100
+}
+
+function wrapAround(x: number, boundary: number) {
+  return (x + boundary) % boundary;
+}
+
 function SegmentView({ circle, spawn, magnification }: SegmentViewProps) {
   const style = {
-    bottom: `${((circle.center.y - circle.radius)*magnification/100 + spawn.y) % window.innerHeight}px`,
-    right: `${((circle.center.x - circle.radius)*magnification/100 + spawn.x) % window.innerWidth}px`,
+    bottom: `${wrapAround(edgeAsPixels(circle.center.y, circle.radius, magnification) + spawn.y, window.innerHeight)}px`,
+    right: `${wrapAround(edgeAsPixels(circle.center.x, circle.radius, magnification) + spawn.x, window.innerWidth)}px`,
     height: `${2*circle.radius*magnification/100}px`,
     width: `${2*circle.radius*magnification/100}px`,
   };
