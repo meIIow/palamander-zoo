23ef47ce16b14818bfeca1d6090b4e44c9c61fa5
meIIow <maxwell.oliver.white@gmail.com>
2025-01-11 21:52:52 -1000

    Configure tailwind CSS framework
    See https://tailwindcss.com/docs/guides/vite

--- client/src/demo/App.tsx
+++ client/src/demo/App.tsx
@@ -8,6 +8,9 @@ function App() {
   return (
     <>
       <h1>Boilerplate Placeholder: Palamander Zoo</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
