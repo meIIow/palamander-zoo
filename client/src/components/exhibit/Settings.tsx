b39ae11dae8eb1d3ec759af5c0b48a97185e3c58
meIIow <maxwell.oliver.white@gmail.com>
2025-01-24 13:51:05 -0800

    Customize updateInterval and magnification

--- client/src/components/exhibit/Settings.tsx
+++ client/src/components/exhibit/Settings.tsx
@@ -5,11 +5,29 @@ function Settings({ settings, customize } : { settings: PalSettings, customize: 
   const customizeColor = (event: ChangeEvent<HTMLInputElement>) => {
     return customize({ ...settings, color: event.target.value });
   };
+  const customizeMagnification = (event: ChangeEvent<HTMLInputElement>) => {
+    return customize({ ...settings, magnification: 10 * Math.pow(5, (parseInt(event.target.value) / 100)) });
+  };
+  const customizeInterval = (event: ChangeEvent<HTMLInputElement>) => {
+    return customize({ ...settings, updateInterval: parseInt(event.target.value) });
+  };
+  const magExponent = Math.log(settings.magnification / 10) / Math.log(5);
   return (
     <div>
       <div>
         Settings
-        <input type="color" value={settings.color} onChange={customizeColor}/>
+        <div>
+          Color
+          <input type="color" value={settings.color} onChange={customizeColor}/>
+        </div>
+        <div>
+          <input type="range" min="-100" value={magExponent*100} max="100" step="20" onChange={customizeMagnification}/>
+          {`Magnification: ${Math.round(settings.magnification * 100) / 1000}x`}
+        </div>
+        <div>
+          <input type="range" min="30" value={settings.updateInterval} max="250" step="20" onChange={customizeInterval}/>
+          {`Interval: ${settings.updateInterval}`}
+        </div>
       </div>
     </div>
   )
